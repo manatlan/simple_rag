48fd8bb8231f802c2f1be5612b824fa0abe82a9b
manatlan <manatlan@gmail.com>
2025-10-25 11:52:50 +0200

    top

--- main.py
+++ main.py
@@ -6,29 +6,28 @@ import dotenv
 dotenv.load_dotenv()  # GEMINI_API_KEY
 
 
-class MyDB:
+class DB:
     collection_name = "docs"
 
-    def __init__(self, docs: list[str]):
-        self.db = chromadb.PersistentClient(path="MyDB")
-        if MyDB.collection_name not in [col.name for col in self.db.list_collections()]:
+    def __init__(self, name:str, docs: list[str]):
+        self.db = chromadb.PersistentClient(path=name)
+        if DB.collection_name not in [col.name for col in self.db.list_collections()]:
             self._feed(docs)
 
     def _feed(self, docs: list[str]):
-        collection = self.db.create_collection(MyDB.collection_name)
+        collection = self.db.create_collection(DB.collection_name)
         for i, text in enumerate(docs):
             collection.add(documents=[text], ids=[str(i)])
 
     def find(self, query: str, nb_docs: int = 2) -> list[str]:
         """recherche dans les documents disponibles, ceux qui te paraissent interessant pour la query 'query', retourne les documents"""
-        qresult = self.db.get_collection(MyDB.collection_name).query(
+        qresult = self.db.get_collection(DB.collection_name).query(
             query_texts=[query], n_results=nb_docs
         )
         if qresult and "documents" in qresult and qresult["documents"]:
             docs_found = qresult["documents"][0]
         else:
             docs_found = []
-        print("::::", query, "-->", docs_found)
         return docs_found
 
 
@@ -40,7 +39,7 @@ if __name__ == "__main__":
         "La tour montparnasse est a bruxelles",
     ]
 
-    db = MyDB(docs)
+    db = DB("MyDB",docs)
 
     agent = Agent(
         model="gemini-2.5-flash",
